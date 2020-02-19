f26191f3b944da6088a82220868eeed5930e04c7
kaiserawu <kaiser.a.wu@gmail.com>
2020-02-18 20:01:54 -0800

    Also commit ancestor implementation

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,52 @@
+import sys
+import os
+sys.path.append(os.path.relpath('../graph'))
+from util import Queue
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+    for pair in ancestors:
+        parent = pair[0]
+        child = pair[1]
+        graph.add_vertex(parent)
+        graph.add_vertex(child)
+        graph.add_edge(child, parent)
+
+    queue = Queue()
+    queue.enqueue([starting_node])
+    
+    longest_path_length = 1
+    earliest_ancestor = -1
+
+    while queue.size() > 0:
+        path = queue.dequeue()
+        current_node = path[-1]
+
+        if len(path) == longest_path_length:
+            if current_node < earliest_ancestor:
+                earliest_ancestor = current_node
+        if len(path) > longest_path_length:
+            longest_path_length = len(path)
+            earliest_ancestor = current_node
+
+        neighbors = graph.vertices[current_node]
+
+        for ancestor in neighbors:
+            path_copy = list(path)
+            path_copy.append(ancestor)
+            queue.enqueue(path_copy)
+
+    return earliest_ancestor
