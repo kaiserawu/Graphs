a3919983b45713894f34751a77c02d3ab2a31e68
kaiserawu <kaiser.a.wu@gmail.com>
2020-02-20 19:28:22 -0800

    Add utility classes and implement intial graph room gen

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -27,6 +27,64 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+class Adv_Graph:
+    def __init__(self):
+        self.rooms = {}
+        self.player = Player(world.starting_room)
+
+    def discover_room(self):
+        room_id = self.player.current_room.id
+        if room_id not in self.rooms:
+            directions = self.player.current_room.get_exits()
+            self.rooms[room_id] = {}
+            for direction in directions:
+                self.rooms[room_id][direction] = '?'
+    
+    def list_all_unexplored(self):
+        curr_room = self.rooms[self.player.current_room.id]
+        unexplored_directions = []
+        for direction in curr_room:
+            if curr_room[direction] == '?':
+                unexplored_directions.append(direction)
+        return unexplored_directions
+    
+    def test(self):
+        self.discover_room()
+        print(self.list_all_unexplored())
+    
+
+adv_graph = Adv_Graph()
+adv_graph.test()
+
+
+
+
 traversal_path = []
 
 
@@ -51,12 +109,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
